e9d9965bf2a56132b86449690c2f2a09b2d48b20
Jeppe BLicher Tarp <raider.j@gmail.com>
2013-09-13 11:28:59 +0200

    assignment 2

--- a_02/views.py
+++ a_02/views.py
@@ -1,7 +1,18 @@
 # Create your views here.
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-
+from django.http import HttpResponse
+from urllib import urlopen
+from lxml.etree import XML, XSLT
 
 def home(request):
-
+    data = urlopen('http://www.cs.washington.edu/research/xmldatasets/data/courses/reed.xml')
+    xml = XML(data.read())
+    data.close()
+    
+    data = urlopen('http://%s/static/xslt/a02.xslt' % (request.get_host(),))
+    xslt = XML(data.read())
+    data.close()
+    
+    transform = XSLT(xslt)
+    return HttpResponse(transform(xml))
